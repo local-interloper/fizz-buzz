81da51c248ca3f39a5faf7a023447252a96ef41a
Antonio <antonio.gaming10@gmail.com>
2022-12-06 21:08:37 +0100

    Added c# FizzBuzz

---
+++ FizzBuzz c#/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Program {
+  static void Main(string[] args) {
+    
+    for (int i = 1; i <= 100; i++){
+      string output = "";
+      if (i % 3 == 0) {
+        output+="Fizz";
+      }
+      if (i % 5 == 0) {
+        output+="Buzz";
+      }
+      if (output == "") {
+        Console.WriteLine(i);
+      } else {
+        Console.WriteLine(output);
+      }
+    }
+  }
+}
